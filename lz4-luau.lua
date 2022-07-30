fd2d57105d4fd1e8cba5d3b3a19a1b982c9a8d06
TFilteredC <thomthom1@live.co.uk>
2022-07-30 22:59:45 +0100

    initial commit (working on a decompressor)

---
+++ lz4-luau.lua
@@ -0,0 +1,190 @@
+local lz4 = {}
+
+local function plainFind(str, pat)
+	return string.find(str, pat, 0, true)
+end
+
+local function streamer(str)
+	local Stream = {}
+	Stream.Offset = 0
+	Stream.Source = str
+	Stream.Length = string.len(str)
+	Stream.IsFinished = false	
+	
+	function Stream:read(len, shift)
+		local len = len or 1
+		local shift = if shift ~= nil then shift else true
+		local dat = string.sub(self.Source, self.Offset + 1, self.Offset + len)
+		
+		if shift then
+			self:seek(len)
+		end
+		
+		return dat
+	end
+	
+	function Stream:seek(len)
+		local len = len or 1
+		
+		self.Offset += math.clamp(len, 0, self.Length)		
+		self.IsFinished = self.Offset >= self.Length
+	end
+	
+	return Stream
+end
+
+function lz4.compress(str)
+	local blocks = {}
+	local iostream = streamer(str)
+	
+	if iostream.Length > 8 then
+		local firstFour = iostream:read(4)
+
+		local processed = firstFour
+		local lit = firstFour
+		local match = ""
+		local LiteralPushValue = ""
+		local pushToLiteral = true
+		
+		repeat
+			pushToLiteral = true
+			local nextByte = iostream:read()
+
+			if plainFind(processed, nextByte) then
+				local next3 = iostream:read(3, false)
+				
+				if string.len(next3) < 3 then
+					--push bytes to literal block then break
+					LiteralPushValue = nextByte .. next3
+					iostream:seek(3)
+				else
+					match = nextByte .. next3
+
+					local matchPos = plainFind(processed, match)
+					if matchPos then
+						iostream:seek(3)
+						repeat
+							local nextMatchByte = iostream:read(1, false)
+							local newResult = match .. nextMatchByte
+
+							local repos = plainFind(processed, newResult) 
+							if repos then
+								match = newResult
+								matchPos = repos
+								iostream:seek(1)
+							end
+						until not plainFind(processed, newResult) or iostream.IsFinished
+
+						local matchLen = string.len(match)
+						local pushMatch = true
+						
+						if iostream.IsFinished then
+							if matchLen <= 4 then
+								LiteralPushValue = match
+								pushMatch = false
+							else
+								matchLen = matchLen - 4
+								match = string.sub(match, 1, matchLen)
+								iostream:seek(-4)
+							end
+						end
+
+						if pushMatch then
+							pushToLiteral = false
+
+							-- gets the position from the end of processed, then slaps it onto processed
+							local realPosition = string.len(processed) - matchPos
+							processed = processed .. match
+
+							table.insert(blocks, {
+								Literal = lit,
+								LiteralLength = string.len(lit),
+								MatchOffset = realPosition + 1,
+								MatchLength = matchLen,
+							})
+							lit = ""
+						end
+					else
+						LiteralPushValue = nextByte
+					end
+				end
+			else
+				LiteralPushValue = nextByte
+			end
+
+			if pushToLiteral then
+				lit = lit .. LiteralPushValue
+				processed = processed .. nextByte
+			end
+		until iostream.IsFinished
+		table.insert(blocks, {
+			Literal = lit,
+			LiteralLength = string.len(lit)
+		})
+	else
+		local str = iostream.Source
+		blocks[1] = {
+			Literal = str,
+			LiteralLength = string.len(str)
+		}
+	end
+
+	-- generate the output chunk
+	-- %s is for adding header
+	local output = string.rep("\x00", 4)
+	local function write(char)
+		output = output .. char
+	end
+	-- begin working through chunks
+	for chunkNum, chunk in blocks do
+		local litLen = chunk.LiteralLength
+		local matLen = (chunk.MatchLength or 4) - 4
+
+		-- create token
+		local tokenLit = math.clamp(litLen, 0, 15)
+		local tokenMat = math.clamp(matLen, 0, 15)
+
+		local token = bit32.lshift(tokenLit, 4) + tokenMat
+		write(string.pack("<I1", token))
+
+		if litLen >= 15 then
+			litLen = litLen - 15
+			--begin packing extra bytes
+			repeat
+				local nextToken = litLen % 256
+				write(string.pack("<I1", nextToken))
+				if nextToken == 0xFF then
+					litLen = litLen - 255
+				end
+			until nextToken < 0xFF
+		end
+
+		-- push raw lit data
+		write(chunk.Literal)
+
+		if chunkNum ~= #blocks then
+			-- push offset as u16
+			write(string.pack("<I2", chunk.MatchOffset))
+
+			-- pack extra match bytes
+			if matLen >= 15 then
+				matLen = matLen - 15
+
+				repeat
+					local nextToken = matLen % 256
+					write(string.pack("<I1", nextToken))
+					if nextToken == 0xFF then
+						matLen = matLen - 255
+					end
+				until nextToken < 0xFF
+			end
+		end
+	end
+	--append chunks
+	local compLen = string.len(output) - 4
+	local decompLen = iostream.Length
+
+	return string.pack("<I4", compLen) .. string.pack("<I4", decompLen) .. output, blocks
+end
+
+return lz4
